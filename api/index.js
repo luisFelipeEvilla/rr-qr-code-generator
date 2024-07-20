34101f5f0b896955b96c67f59e1768a1d23b2ab4
Luis Felipe Evilla Rodriguez <luisevilla588@gmail.com>
2024-07-20 07:24:39 -0500

    deploymnt to vercel

--- api/index.js
+++ api/index.js
@@ -1,61 +1,27 @@
 const express = require("express");
 const qrCode = require("qrcode");
-const stream = require("stream");
-const qr = require("qr-image");
-const fs = require("fs");
 const PDFDocument = require("pdfkit");
 
 const app = express();
 
 const PORT = process.env.PORT || 3000;
 
-// app.get('/', async (req, res) => {
-//    // generate a QR code
-//    const url = 'https://inventario-v2.mrconsulting.com.co/fichatecnica/generate?codigo=4196';
-//    const qrCodeImage = await qrCode.toDataURL(url);
-//     res.send(`<img src="${qrCodeImage}" alt="QR Code"/>`)
-// });
+app.use(express.json());
 
 app.get("/", (req, res) => {
-  // obtener search params;
-//   const { codigo, consecutivo } = req.query;
-
-//   // Generar el código QR
-//   const qr_png = qr.imageSync(
-//     `https://inventario-v2.mrconsulting.com.co/fichatecnica/generate?codigo=${consecutivo}`,
-//     { type: "png" }
-//   );
-
-//   // Crear un nuevo documento PDF
-//   const doc = new PDFDocument();
-//   const outputFilename = `${consecutivo}-${codigo}.pdf`;
-
-//   // Stream del PDF hacia la respuesta HTTP
-//   res.setHeader(
-//     "Content-Disposition",
-//     `attachment; filename="${outputFilename}"`
-//   );
-//   doc.pipe(res);
-
-//   // Insertar el código QR en el PDF
-//   doc.image(qr_png, { fit: [250, 250] });
-
-//   // Finalizar el documento PDF
-//   doc.end();
-
     res.send("QR Generator service");
 });
 
-app.get("/generate", async (req, res) => {
+app.post("/", async (req, res) => {
   // obtener search params;
-  let { cliente, consecutivo, inicio, fin } = req.query;
+  let { cliente, consecutivo, inicio, fin } = req.body;
   // Verificar que los parámetros de inicio y fin sean números enteros
-  if (!Number.isInteger(Number(inicio)) || !Number.isInteger(Number(fin))) {
-    res
-      .status(400)
-      .send("Los parámetros de inicio y fin deben ser números enteros");
-    return;
-  }
+  // if (!Number.isInteger(Number(inicio)) || !Number.isInteger(Number(fin))) {
+  //   res
+  //     .status(400)
+  //     .send("Los parámetros de inicio y fin deben ser números enteros");
+  //   return;
+  // }
 
   if (!cliente || cliente === "") {
     res.status(400).send("El parámetro de cliente es requerido");
